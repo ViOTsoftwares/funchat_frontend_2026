ed2b9def99a0ebbe16cdad06a52d9791942fc5b0
VigneshR3 <vignesh2003rajendran@gmail.com>
2026-06-13 13:35:09 +0530

    env

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,5 +1,5 @@
-﻿export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
-
+﻿import { ENV } from "../config/env";
+export const BACKEND_URL = ENV.API_URL;
 export const ICE_CONFIG = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
 };
